11c11cf73a0342f4f4e44631dd2281f63d62550f
Roman Golovachev <rrgolovachev@yandex-team.ru>
2024-12-19 22:41:46 +0300

    add limiting

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -26,6 +26,9 @@ owner = contract.functions.owner().call()
 rate = contract.functions.exchangeRate().call()
 nonce = web3.eth.get_transaction_count(BOT_ADDRESS)
 
+active_users_lock = threading.Lock()
+active_users = set()
+
 print(f"owner: {owner}, rate: {rate}")
 
 # Telegram bot setup
@@ -49,6 +52,7 @@ dice_coefs = {
     '🎰': [0 for i in range(64)]
 }
 
+
 dice_coefs['🎰'][0] = 9 # bar x3
 dice_coefs['🎰'][63] = 30 # 777
 dice_coefs['🎰'][21] = 9 # grape x3
@@ -58,6 +62,7 @@ dice_coefs['🎰'][42] = 9 # lemon x3
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.reply_to(message, "Welcome to the Casino Bot! Use /register <your_wallet_address> to deposit CasinoToken and start playing.")
+
 
 @bot.message_handler(commands=["register"])
 def register(message):
@@ -81,6 +86,7 @@ def register(message):
 
     bot.reply_to(message, registered_msg)
 
+
 @bot.message_handler(commands=["balance"])
 def balance(message):
     user, wallet_address = login(message)
@@ -94,45 +100,56 @@ def balance(message):
 
     bot.reply_to(message, balance_msg)
 
+
 @bot.message_handler(commands=["roll_dice"])
 def roll_dice(message):
     message.text = '/roll 🎲'
     return roll(message)
+
 
 @bot.message_handler(commands=["roll_basket"])
 def roll_basket(message):
     message.text = '/roll 🏀'
     return roll(message)
 
+
 @bot.message_handler(commands=["roll_darts"])
 def roll_darts(message):
     message.text = '/roll 🎯'
     return roll(message)
+
 
 @bot.message_handler(commands=["roll_bowling"])
 def roll_bowling(message):
     message.text = '/roll 🎳'
     return roll(message)
 
+
 @bot.message_handler(commands=["roll_kazik"])
 def roll_kazik(message):
     message.text = '/roll 🎰'
     return roll(message)
+
 
 @bot.message_handler(commands=["roll"])
 def roll(message):
     user, wallet_address = login(message)
     if not user or not wallet_address:
         return
+    if not add_user(wallet_address):
+        bot.reply_to(message, "Please do not hurry")
+        return
 
     args = message.text.split()
     if len(args) != 2:
         bot.reply_to(message, "Please provide casino type as an emoji. Examples: 🎲, 🏀")
+        remove_user(wallet_address)
         return
 
     emoji = args[1]
     if emoji not in dice_coefs:
         bot.reply_to(message, f"Unsupportable emoji provided. Avalable emojies: {', '.join(dice_coefs.keys())}")
+        remove_user(wallet_address)
         return
     coefs = dice_coefs[emoji]
 
@@ -143,6 +160,7 @@ def roll(message):
         not_enough_bal_msg += f"Send ETH (0.005 ETH = 1 SMPH, 10 SMPTH = 1 roll) to the following address to start playing: {CONTRACT_ADDRESS}"
 
         bot.reply_to(message, not_enough_bal_msg)
+        remove_user(wallet_address)
         return
 
     tx_hash = send_token(wallet_address, CONTRACT_ADDRESS, TOKENS_TO_ROLL)
@@ -152,6 +170,7 @@ def roll(message):
 
     if tx_receipt['status'] != 1:
         bot.reply_to(message, f"Transaction for wei failed: [tx](https://sepolia.etherscan.io/tx/0x{tx_hash.hex()} :( Please try again", disable_web_page_preview=True, parse_mode="markdown")
+        remove_user(wallet_address)
         return
 
     dice_response = bot.send_dice(message.chat.id, emoji=emoji)
@@ -165,34 +184,44 @@ def roll(message):
         bot.reply_to(message, f"You win {payout} semaphore tokens! The prize will be sent as soon as possible. [tx](https://sepolia.etherscan.io/tx/0x{tx_hash.hex()})", disable_web_page_preview=True, parse_mode="markdown")
     else:
         bot.reply_to(message, "No payout this time. Better luck next roll!")
+    remove_user(wallet_address)
+
 
 @bot.message_handler(commands=["withdraw"])
 def withdraw(message):
     user, wallet_address = login(message)
     if not user or not wallet_address:
         return
+    if not add_user(wallet_address):
+        bot.reply_to(message, "Please do not hurry")
+        return
 
     args = message.text.split()
 
     if len(args) != 2:
         bot.reply_to(message, "Please provide your withdraw amount. Usage: /withdraw <amount of SMPH tokens>")
+        remove_user(wallet_address)
         return
 
     if not args[1].isdigit():
         bot.reply_to(message, "Amount of tokens must be integer")
+        remove_user(wallet_address)
         return
     withdraw_amount = int(args[1])
     # print(wallet_address, withdraw_amount)
 
     balance = contract.functions.balanceOf(wallet_address).call()
     if balance < withdraw_amount:
-        not_enough_bal_msg = f"You don't have enough money to withdraw {withdraw_amount}.\n Current balance: {balance}\n"
+        not_enough_bal_msg = f"You don't have enough money to withdraw {withdraw_amount}.\nCurrent balance: {balance}\n"
         bot.reply_to(message, not_enough_bal_msg)
+        remove_user(wallet_address)
         return
 
     tx_hash = withdraw_tokens(wallet_address, withdraw_amount)
     # threading.Thread(target=send_eth, args=(wallet_address, withdraw_amount), daemon=True).start()
     bot.reply_to(message, f"Withdraw success! The ETH will be sent as soon as possible. [tx](https://sepolia.etherscan.io/tx/0x{tx_hash.hex()})", disable_web_page_preview=True, parse_mode="markdown")
+    remove_user(wallet_address)
+
 
 def login(message):
     user = message.from_user
@@ -204,6 +233,7 @@ def login(message):
 
     return user, wallet_address
 
+
 def get_tx_receipt(tx_hash):
     transaction_receipt = None
     while transaction_receipt is None:
@@ -213,6 +243,7 @@ def get_tx_receipt(tx_hash):
             pass
         time.sleep(0.1)
     return transaction_receipt
+
 
 def send_token(sender, receiver, amount):
     global nonce
@@ -228,6 +259,7 @@ def send_token(sender, receiver, amount):
 
     return tx_hash
 
+
 def withdraw_tokens(receiver, amount):
     global nonce
     tx = contract.functions.exchangeTokens(amount, receiver).build_transaction({
@@ -242,6 +274,7 @@ def withdraw_tokens(receiver, amount):
 
     return tx_hash
 
+
 def send_prize(wallet_address, payout):
     was_sent = False
     while not was_sent:
@@ -249,11 +282,32 @@ def send_prize(wallet_address, payout):
         tx_receipt = get_tx_receipt(tx_hash)
         was_sent = tx_receipt['status'] == 1
 
+
 def send_eth(wallet_address, withdraw_amount):
     was_sent = False
     while not was_sent:
         tx_hash = withdraw_tokens(wallet_address, withdraw_amount)
         tx_receipt = get_tx_receipt(tx_hash)
         was_sent = tx_receipt['status'] == 1
+
+
+def add_user(wallet):
+    global active_users_lock
+    global active_users
+
+    with active_users_lock:
+        if wallet in active_users:
+            return False
+        active_users.add(wallet)
+        return True
+
+
+def remove_user(wallet):
+    global active_users_lock
+    global active_users
+
+    with active_users_lock:
+        active_users.discard(wallet)
+
 
 bot.polling()
